0f67fd8a51b3cdea62c7ca3df730ef5fb4097e53
Violet :3 <153395462+Entrpix@users.noreply.github.com>
2023-12-31 17:59:38 -0500

    Delete config directory

--- config/preferedNames.js
+++
@@ -1,9 +0,0 @@
-const userPreferences = {
-    "1168045766770696193":"Entrpix",
-    "366312087317774336": "walkeast",
-    "247349845298249728": "flowergirl",
-    "578375908247863296":"riotboy",
-    "927240253272645662":"coalforchristmas"
-};
-
-module.exports = userPreferences;
