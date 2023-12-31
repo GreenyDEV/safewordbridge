8ebc3e4736cdf825e62fc07696214d77d3e2427e
Violet :3 <153395462+Entrpix@users.noreply.github.com>
2023-12-31 18:00:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
+const { httpPort, frontend, groupDM_ID, token, disc_status, disc_activity, disc_activity_status } = require('./config');
+
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 const Discord = require("discord.js-selfbot");
 const path = require("path");
 var colors = require('@colors/colors');
-const httpPort = 3000;
 
 const app = express();
 const httpServer = http.createServer(app);
 const io = socketio(httpServer);
 
 const client = new Discord.Client();
-const token = ""; 
 
-const directory = path.join(__dirname, "public");
-
+const directory = path.join(__dirname, `${frontend}`);
 app.use(express.static(directory));
 
 const rooms = {};
 const usernames = {};
-const groupChatChannelId = "1078485480439550023"; // Replace with your group chat channel ID
-const userPreferences = require('./config/preferedNames')
+const userPreferences = require('./config')
 const tagToPreferred = require('./lib/utility/tagToPrefered')
 const iDToPreferred = require('./lib/utility/idToPrefered')
 const replaceWithUser = require('./lib/utility/mentionConversion')
-
-
 
  client.on("ready", async () => {
     console.log(colors.bgGreen.black("[ ✔ ]") + `${colors.green(" Safebridge successfully Logged in as")} ${colors.bgBrightYellow.black.bold(client.user.tag)}`);
     console.log(colors.bgGreen.black("[ ✔ ] READY"))
 
     client.user.setPresence({
-        status: 'dnd', // You can use 'online', 'idle', 'dnd' (do not disturb), or 'invisible'
+        status: `${disc_status}`,
         activity: {
-          name: 'SafeWord Bridge',
-          type: 'PLAYING', // You can use 'PLAYING', 'LISTENING', 'WATCHING', 'STREAMING'
+          name: `${disc_activity}`,
+          type: `${disc_activity_status}`,
         },
       });
   });
   
 io.on("connection", function (socket) {
-  socket.on("send", require('./lib/handlers/socket/send')(socket, client, groupChatChannelId));
+  socket.on("send", require('./lib/handlers/socket/send')(socket, client, groupDM_ID));
   socket.on("recieve", require('./lib/handlers/socket/receive')(socket));
-  socket.on("typing", require('./lib/handlers/socket/typing')(socket, client, groupChatChannelId));
+  socket.on("typing", require('./lib/handlers/socket/typing')(socket, client, groupDM_ID));
 }); 
 
-client.on("message", require("./lib/handlers/client/message")(client, groupChatChannelId, io));
+client.on("message", require("./lib/handlers/client/message")(client, groupDM_ID, io));
 client.login(token);
 
 
-httpServer.listen(httpPort, () => {
+httpServer.listen(`${httpPort}`, () => {
     console.log(colors.bgGreen.black("[ ✔ ]") + colors.bold.green(" HTTP Server mounted on port ") + colors.brightGreen.underline(httpPort))
 
 });
